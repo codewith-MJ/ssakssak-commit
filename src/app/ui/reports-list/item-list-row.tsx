68b6d03811c0bf660e50107081fab801127301f6
codewith-MJ <minji.regina.kim@gmail.com>
2025-11-22 17:24:13 +0900

    ♻️refactor: 리포트 목록 row 전체 클릭 가능하도록 UX 개선

--- src/app/ui/reports-list/item-list-row.tsx
+++ src/app/ui/reports-list/item-list-row.tsx
@@ -1,45 +1,40 @@
+"use client";
+
 import { ReportListItem } from "@/types/report";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 type ReportsTableRowProps = {
   item: ReportListItem;
 };
 
 function ReportsTableRow({ item }: ReportsTableRowProps) {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push(`/report-view/${item.reportId}`);
+  };
+
   return (
-    <tr className="cursor-pointer text-base text-gray-700 transition-colors hover:bg-gray-50">
-      <td className="px-5 py-3 align-middle">
-        <Link href={`/reports/${item.reportId}`} className="block">
-          <div className="truncate whitespace-nowrap" title={item.reportTitle}>
-            <span className="text-[15px] font-medium text-gray-500">
-              {item.reportTitle}
-            </span>
-          </div>
-        </Link>
-      </td>
-
-      <td className="px-3 py-3 align-middle">
-        <div
-          className="truncate whitespace-nowrap text-gray-500"
-          title={`${item.owner} / ${item.repositoryName}`}
-        >
-          {item.owner} / {item.repositoryName}
-        </div>
-      </td>
-
-      <td className="px-3 py-3 align-middle">
-        <div
-          className="truncate whitespace-nowrap text-gray-500"
-          title={item.branch}
-        >
-          {item.branch}
-        </div>
-      </td>
-
-      <td className="px-5 py-3 text-center align-middle">
-        <span className="text-sm whitespace-nowrap text-gray-500">
-          {item.createdAt}
+    <tr
+      onClick={handleClick}
+      className="cursor-pointer text-base text-gray-700 transition-colors hover:bg-gray-50"
+    >
+      <td className="truncate px-5 py-3 align-middle whitespace-nowrap">
+        <span className="text-[15px] font-medium text-gray-500">
+          {item.reportTitle}
         </span>
+      </td>
+
+      <td className="truncate px-3 py-3 align-middle whitespace-nowrap text-gray-500">
+        {item.owner} / {item.repositoryName}
+      </td>
+
+      <td className="truncate px-3 py-3 align-middle whitespace-nowrap text-gray-500">
+        {item.branch}
+      </td>
+
+      <td className="px-5 py-3 text-center align-middle text-sm whitespace-nowrap text-gray-500">
+        {item.createdAt}
       </td>
     </tr>
   );
